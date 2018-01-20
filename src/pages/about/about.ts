8551edb611a04a49f7f8d376fd3d1302f5a31c4e
Raja Kedia(miyagix) <rajakedia2222@gmail.com>
2018-01-20 10:14:00 +0530

    Assignment 1 Complete

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Leader } from '../../shared/leader';
+import { LeaderProvider } from '../../providers/leader/leader';
 
 /**
  * Generated class for the AboutPage page.
@@ -15,11 +18,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    leaders: Leader[];
+    errMsg: string;
+    
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private leaderservice: LeaderProvider, @Inject('BaseURL') private BaseURL) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
   }
+    ngOnInit(){
+        this.leaderservice.getLeaders()
+        .subscribe(leader => this.leaders = leader, errmess => this.errMsg = <any>errmess);
+    }
 
 }
